7165bc22453842c2f5c471db43906a5ca617bd06
Shanavas <88940105+MShanavas@users.noreply.github.com>
2023-04-28 12:42:25 +0530

    completed variables

--- variables.tf
+++ variables.tf
@@ -1,34 +1,37 @@
 variable "access_key" {
-        description = "Access key to AWS console"
+  description = "Access key to AWS console"
 }
 
 variable "secret_key" {
-        description = "Secret key to AWS console"
+  description = "Secret key to AWS console"
 }
 
 variable "ami" {
-        default = "ami-0578f2b35d0328762"
-        description = "Image should be available on the same region"
+  default     = "ami-0578f2b35d0328762"
+  description = "Image should be available on the same region"
 }
 
 variable "instance_name" {
-        description = "Name of the instance to be created"
-        default = "srv01"
+  description = "Name of the instance to be created"
+  default     = "srv01"
 }
 
 variable "instance_type" {
-        description = "instance type"
-        default = "t2.micro"
+  description = "instance type"
+  default     = "t2.micro"
 }
 variable "aws_s3_bucket_name" {
-        default = "techtvm-s3"
-        description = "Name of the Bucket"
+  default     = "techtvm-s3"
+  description = "Name of the Bucket"
 }
 variable "environment" {
-        default = "Prod"
-        description = "Environment type"
+  default     = "Prod"
+  description = "Environment type"
 }
 variable "project_name" {
-        default = "TechTVM"
-        description = "Name of the project"
+  default     = "TechTVM"
+  description = "Name of the project"
+}
+variable "rds_dbsecret" {
+  description = "RDS secret password of dbadmin of the tech_tvmdb"
 }
